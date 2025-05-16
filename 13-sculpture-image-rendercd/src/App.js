4877ec474e27b28b7db49f846c0705f647bcc5e6
Ismail <ghvh8176@gmail.com>
2025-05-17 02:41:16 +0600

    add previous

--- 13-sculpture-image-rendercd/src/App.js
+++ 13-sculpture-image-rendercd/src/App.js
@@ -1,10 +1,56 @@
-import Gallery from './Gallery.js';
+import { useState } from 'react';
+import { sculptureList } from './data.js';
 
-export default function Page() {
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const [previous, setPrevious] = useState(0);
+  let hasPrev = index > 0;
+  let hasNext = index < sculptureList.length - 1;
+
+  function handlePrevClick() {
+    if (hasPrev) {
+      setIndex(index - 1);
+    }
+  }
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+ 
+
+  let sculpture = sculptureList[index];
   return (
-    <div className="Page">
-      <Gallery />
-      <Gallery />
-    </div>
+    <>
+      <button onClick={handlePrevClick}>
+        Previous
+      </button>
+     
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+    </>
   );
 }
