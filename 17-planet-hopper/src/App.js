54856699155328ecf908c18f80b52857861adca1
Ismail <ghvh8176@gmail.com>
2025-05-18 19:14:11 +0600

    add button

--- 17-planet-hopper/src/App.js
+++ 17-planet-hopper/src/App.js
@@ -1,29 +1,26 @@
-import { useState } from 'react';
+import { useImmer } from 'use-immer';
 import { initialTravelPlan } from './places.js';
 
-function PlaceTree({ id, placesById }) {
-  const place = placesById[id];
-  const childIds = place.childIds;
-  return (
-    <li>
-      {place.title}
-      {childIds.length > 0 && (
-        <ol>
-          {childIds.map(childId => (
-            <PlaceTree
-              key={childId}
-              id={childId}
-              placesById={placesById}
-            />
-          ))}
-        </ol>
-      )}
-    </li>
-  );
-}
-
 export default function TravelPlan() {
-  const [plan, setPlan] = useState(initialTravelPlan);
+  const [plan, updatePlan] = useImmer(initialTravelPlan);
+
+  function handleComplete(parentId, childId) {
+    updatePlan(draft => {
+      // Remove from the parent place's child IDs.
+      const parent = draft[parentId];
+      parent.childIds = parent.childIds
+        .filter(id => id !== childId);
+
+      // Forget this place and all its subtree.
+      deleteAllChildren(childId);
+      function deleteAllChildren(id) {
+        const place = draft[id];
+        place.childIds.forEach(deleteAllChildren);
+        delete draft[id];
+      }
+    });
+  }
+
   const root = plan[0];
   const planetIds = root.childIds;
   return (
@@ -34,10 +31,40 @@ export default function TravelPlan() {
           <PlaceTree
             key={id}
             id={id}
+            parentId={0}
             placesById={plan}
+            onComplete={handleComplete}
           />
         ))}
       </ol>
     </>
+  );
+}
+
+function PlaceTree({ id, parentId, placesById, onComplete }) {
+  const place = placesById[id];
+  const childIds = place.childIds;
+  return (
+    <li>
+      {place.title}
+      <button onClick={() => {
+        onComplete(parentId, id);
+      }}>
+        Complete
+      </button>
+      {childIds.length > 0 &&
+        <ol>
+          {childIds.map(childId => (
+            <PlaceTree
+              key={childId}
+              id={childId}
+              parentId={id}
+              placesById={placesById}
+              onComplete={onComplete}
+            />
+          ))}
+        </ol>
+      }
+    </li>
   );
 }
--- 17-planet-hopper/src/index.js
+++ 17-planet-hopper/src/index.js
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./App.css";
+import { useImmer } from 'use-immer';
+
 
 import App from "./App";
 
