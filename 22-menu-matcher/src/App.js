9832aa8247b2f3833f16ab4cfca07f5448c4463f
Ismail <ghvh8176@gmail.com>
2025-05-19 20:00:25 +0600

    filter

--- 22-menu-matcher/src/App.js
+++ 22-menu-matcher/src/App.js
@@ -2,28 +2,32 @@ import { useState } from 'react';
 import { foods, filterItems } from './data.js';
 
 export default function FilterableList() {
-  return (
-    <>
-      <SearchBar />
-      <hr />
-      <List items={foods} />
-    </>
-  );
-}
-
-function SearchBar() {
   const [query, setQuery] = useState('');
+  const results = filterItems(foods, query);
 
   function handleChange(e) {
     setQuery(e.target.value);
   }
 
   return (
+    <>
+      <SearchBar
+        query={query}
+        onChange={handleChange}
+      />
+      <hr />
+      <List items={results} />
+    </>
+  );
+}
+
+function SearchBar({ query, onChange }) {
+  return (
     <label>
       Search:{' '}
       <input
         value={query}
-        onChange={handleChange}
+        onChange={onChange}
       />
     </label>
   );
@@ -32,7 +36,7 @@ function SearchBar() {
 function List({ items }) {
   return (
     <table>
-      <tbody>
+      <tbody> 
         {items.map(food => (
           <tr key={food.id}>
             <td>{food.name}</td>
