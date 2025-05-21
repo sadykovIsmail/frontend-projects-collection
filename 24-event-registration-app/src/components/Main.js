43a7f7ac936ec37d7512da00e5c7d45b33ec36ed
Ismail <ghvh8176@gmail.com>
2025-05-21 21:56:34 +0600

    main

---
+++ 24-event-registration-app/src/components/Main.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import AttendeeInfo from "./AttendeeInfo";
+import ChooseTickets from "./ChooseTickets";
+import AdditionalDetails from "./AdditionalDetails";
+import '../styles/Main.css'
+
+export default function Main () {
+    const[attendData, setAttendData] =useState(null)
+    const[ticketsData, setTicketsData] = useState(null)
+    const[additionalData, setAdditionalData] = useState(null)
+    return(
+        <div className="Main">
+            <h1>Event Registration</h1>
+<AttendeeInfo data={attendData} onSubmit={setAttendData}/>
+<ChooseTickets data={ticketsData} onSubmit={setTicketsData}/>
+<AdditionalDetails data={additionalData} onSubmit={setAdditionalData}/>
+        </div>
+    )
+}
