5027b89101ba46728ca1959e179be3afc41834b6
Ismail <ghvh8176@gmail.com>
2025-05-21 08:05:54 +0600

    do input

---
+++ 24-event-registration-app/src/components/InputFields.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '.../styles/InputFields.css'
+
+export default function InputField ({
+label, name, type = 'text', value, onChange
+}){
+    return (
+        <label className="inputFIld">
+<span className="label-text">
+{label}
+</span>
+<input
+name={name}
+value={value}
+type={type}
+onChange={onChange}
+/>
+</label>
+    )
+}
