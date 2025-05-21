def68c9a6acacc8b2adca0ad4881bdafbb98dcbb
Ismail <ghvh8176@gmail.com>
2025-05-21 08:08:42 +0600

    clear

--- 24-event-registration-app/src/components/SectionControls.js
+++ 24-event-registration-app/src/components/SectionControls.js
@@ -2,9 +2,9 @@ import React from "react";
 import '.../styles/SectionControls.css'
 
 export default function ({isEditing, onEdit, onSubmit}){
-{isEditing ? <button onClick={onSubmit}>
+return isEditing ? <button onClick={onSubmit}>
     Submit
 </button> : <button onClick={onEdit}>
     Edit
-</button>}
+</button>
 }
