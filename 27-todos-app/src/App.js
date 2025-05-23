0ddd7165f701608afde20a40cf14cfe0d8064e43
Ismail <ghvh8176@gmail.com>
2025-05-23 08:25:22 +0600

    clear useEffect

--- 27-todos-app/src/App.js
+++ 27-todos-app/src/App.js
@@ -1,11 +1,19 @@
-import { useState } from 'react';
-import { initialTodos, createTodo } from './todos.js';
+import { useState, useMemo } from 'react';
+import { initialTodos, createTodo, getVisibleTodos } from './todos.js';
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
   const [showActive, setShowActive] = useState(false);
-  const activeTodos = todos.filter(todo => !todo.completed);
-  const visibleTodos = showActive ? activeTodos : todos;
+  const [text, setText] = useState('');
+  const visibleTodos = useMemo(
+    () => getVisibleTodos(todos, showActive),
+    [todos, showActive]
+  );
+
+  function handleAddClick() {
+    setText('');
+    setTodos([...todos, createTodo(text)]);
+  }
 
   return (
     <>
@@ -17,7 +25,10 @@ export default function TodoList() {
         />
         Show only active todos
       </label>
-      <NewTodo onAdd={newTodo => setTodos([...todos, newTodo])} />
+      <input value={text} onChange={e => setText(e.target.value)} />
+      <button onClick={handleAddClick}>
+        Add
+      </button>
       <ul>
         {visibleTodos.map(todo => (
           <li key={todo.id}>
@@ -25,27 +36,6 @@ export default function TodoList() {
           </li>
         ))}
       </ul>
-      <footer>
-        {activeTodos.length} todos left
-      </footer>
-    </>
-  );
-}
-
-function NewTodo({ onAdd }) {
-  const [text, setText] = useState('');
-
-  function handleAddClick() {
-    setText('');
-    onAdd(createTodo(text));
-  }
-
-  return (
-    <>
-      <input value={text} onChange={e => setText(e.target.value)} />
-      <button onClick={handleAddClick}>
-        Add
-      </button>
     </>
   );
 }
--- 27-todos-app/src/todos.js
+++ 27-todos-app/src/todos.js
@@ -1,4 +1,12 @@
 let nextId = 0;
+let calls = 0;
+
+export function getVisibleTodos(todos, showActive) {
+  console.log(`getVisibleTodos() was called ${++calls} times`);
+  const activeTodos = todos.filter(todo => !todo.completed);
+  const visibleTodos = showActive ? activeTodos : todos;
+  return visibleTodos;
+}
 
 export function createTodo(text, completed = false) {
   return {
