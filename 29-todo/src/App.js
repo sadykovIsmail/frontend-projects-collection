4e715a6771b136085025ee30fa944b7d75f95b41
Ismail <ghvh8176@gmail.com>
2025-05-27 02:00:04 +0600

    main

--- 29-todo/src/App.js
+++ 29-todo/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import FunctionalInput from './components/FunctionalInput';
+import ClassInput from './components/ClassInput';
+import './style.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FunctionalInput name="Functional component!" />
+      <div className="divider" />
+      <ClassInput name="Class based component!" />
+    </>
   );
 }
-
-export default App;
