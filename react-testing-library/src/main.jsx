96a18a7cd0cf74191c64af349b61c41227d6dcde
Ismail <ghvh8176@gmail.com>
2025-05-29 12:07:00 -0500

    rewrite main

--- react-testing-library/src/main.jsx
+++ react-testing-library/src/main.jsx
@@ -1,8 +1,10 @@
-import { StrictMode } from "react";
+import { StrictMode } from "react"
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Profile from "./Profile";
+import Spinach from "./Spinach";
+import Popeye from "./Popeye";
 
 const router = createBrowserRouter([
   {
@@ -12,11 +14,15 @@ const router = createBrowserRouter([
   {
     path: "profile",
     element: <Profile />,
+    children: [
+      { path: "spinach", element: <Spinach /> },
+      { path: "popeye", element: <Popeye /> },
+    ],
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
