d5521eb9e3ae8e95d25c267b5a35ba77900db89a
Ismail <ghvh8176@gmail.com>
2025-05-29 12:29:22 -0500

    rewrite main

--- react-router-demo/src/main.jsx
+++ react-router-demo/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Profile from './Profile';
+import Popeye from './Popeye';
+import Spinach from './Spinach';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'profile',
+    element: <Profile />,
+    children: [
+      { path: 'spinach', element: <Spinach /> },
+      { path: 'popeye', element: <Popeye /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
