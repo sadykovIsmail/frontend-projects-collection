8f179504c2b01a84cd6104982dea35038a744db3
Ismail <ghvh8176@gmail.com>
2025-05-29 13:13:13 -0500

    add to main

--- react-router-demo/src/main.jsx
+++ react-router-demo/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Profile from "./Profile";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "profile/:name",
