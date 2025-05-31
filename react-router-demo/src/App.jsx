907942525b324fd3324a73f411ca39d50b698036
Ismail <ghvh8176@gmail.com>
2025-05-31 11:52:38 -0500

    rectify

--- react-router-demo/src/App.jsx
+++ react-router-demo/src/App.jsx
@@ -1,13 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const App = () => (
-  <div>
-    <h1>Hello from the main page!</h1>
-    <p>Here are some links:</p>
-    <ul>
-      <li><Link to="/profile">Go to Profile</Link></li>
-    </ul>
-  </div>
-);
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li><Link to="/profile/popeye">Popeye Profile</Link></li>
+          <li><Link to="/profile/spinach">Spinach Profile</Link></li>
+          <li><Link to="/profile/unknown">Unknown Profile</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default App;
