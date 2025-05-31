b2503e7ccde125122ad2c484f5d24b8ab617e2f9
Ismail <ghvh8176@gmail.com>
2025-05-31 12:49:09 -0500

    do current

---
+++ 31-my-page/src/Current.jsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom"
+
+const Current = () => {
+    <>
+    <h2>I am studying in NAU</h2>
+    <h4>additionally learning coding from online resources</h4>
+    <Link>Learning online</Link>
+    <Link>Go to the main page</Link>
+    </>
+}
