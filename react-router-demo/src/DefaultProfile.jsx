53790fbae6bbfbcd28d9a31cd6ca6df560645d24
Ismail <ghvh8176@gmail.com>
2025-05-31 12:05:57 -0500

    fix bug

--- react-router-demo/src/DefaultProfile.jsx
+++ react-router-demo/src/DefaultProfile.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const DefaultProfile = () => {
-  return <p>Oh, nothing to see here!</p>;
+  return (
+    <>
+      <p>Oh, nothing to see here!</p>
+      <Link to="/">Click here to go back</Link>
+    </>
+  );
 };
 
 export default DefaultProfile;
