9c2374ed35208c4874b40e5440725ac5db46edf7
Ismail <ghvh8176@gmail.com>
2025-06-01 11:27:35 -0500

    do profile

--- 31-my-page/src/Profile.jsx
+++ 31-my-page/src/Profile.jsx
@@ -1,1 +1,25 @@
 import { useParams } from "react-router-dom";
+import Plans from "./Plans";
+import Current from "./current";
+import Biography from "./Biography";
+import ErrorPage from "./ErrorPage";
+
+const Profile = () => {
+    const { name } = useParams()
+
+    return(
+        <div>
+            <h1>Hello</h1>
+            <hr />
+            {name === "biography" ? (
+                <Biography />
+            ) : name === "current" ? (
+                <Current />
+            ) : name === "plans" ? (
+                <Plans />
+            ) : <ErrorPage />}
+        </div> 
+    )
+}
+
+export default Profile
