396e14ff8e571cf81ca967e160500857cb72e890
Ismail <ghvh8176@gmail.com>
2025-06-01 10:42:01 -0500

    errorpage

---
+++ 31-my-page/src/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+    <div>
+        <h2>Oh no, this link doesn't exists</h2>
+        <Link>Click here to go to the main page</Link>
+    </div>
+}
+
+export default ErrorPage
