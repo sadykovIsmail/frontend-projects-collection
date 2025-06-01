7a576e6552174ea5e8b3f82ffb011098fc04c5e2
Ismail <ghvh8176@gmail.com>
2025-06-01 10:27:38 -0500

    do biography

---
+++ 31-my-page/src/Biography.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Biography = () => {
+    <>
+    <h2>My name is Ismail</h2>
+    <h4>I was born in 2007 in the Osh city of Kyrgyzstan.</h4>
+    <h4>I have graduated the school name: 40th years of Kyrgyzstan</h4>
+    <Link>What am I doing currently</Link>
+    <Link>Go to the main page</Link>
+    </>
+
+}
--- 31-my-page/src/Current.jsx
+++ 31-my-page/src/Current.jsx
@@ -3,8 +3,10 @@ import {Link} from "react-router-dom"
 const Current = () => {
     <>
     <h2>I am studying in NAU</h2>
-    <h4>additionally learning coding from online resources</h4>
-    <Link>Learning online</Link>
+    <h4>additionally learning from online resources</h4>
+    <Link>Go to my online courses</Link>
     <Link>Go to the main page</Link>
     </>
 }
+
+export default Current
