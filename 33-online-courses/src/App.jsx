e00d1159d267e582ef82418ac5a01fba77025c3c
Ismail <ghvh8176@gmail.com>
2025-06-11 16:31:26 -0500

    route history page

--- 33-online-courses/src/App.jsx
+++ 33-online-courses/src/App.jsx
@@ -2,14 +2,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import CoursesPage from "./pages/CoursesPage";
+import EnrolledHistory from "./pages/EnrolledHistory";
+import { useState } from "react";
 
 export default function App() {
+  const [enrolled, setEnrolled] = useState([]);
+
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/courses" element={<CoursesPage />} />
+        <Route path="/courses" element={
+          <CoursesPage enrolled={enrolled} setEnrolled={setEnrolled} />
+        } />
+        <Route path="/history" element={
+          <EnrolledHistory enrolled={enrolled} />
+        } />
       </Routes>
     </BrowserRouter>
   );
