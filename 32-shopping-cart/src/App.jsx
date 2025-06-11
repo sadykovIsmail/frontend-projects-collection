969c6874c28a80a24c45501803ecaf48e6720d63
Ismail <ghvh8176@gmail.com>
2025-06-11 15:11:41 -0500

    app

--- 32-shopping-cart/src/App.jsx
+++ 32-shopping-cart/src/App.jsx
@@ -1,17 +1,39 @@
 // App.jsx
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ShopPage from "./pages/ShopPage";
-import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ShopPage from "./pages/ShopPage";
+import CartPage from "./pages/CartPage";
 
-export default function App() {
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product, quantity) => {
+    setCart(prev => {
+      const existing = prev.find(item => item.id === product.id);
+      if (existing) {
+        return prev.map(item =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + quantity }
+            : item
+        );
+      } else {
+        return [...prev, { ...product, quantity }];
+      }
+    });
+  };
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar cart={cart} />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/shop" element={<ShopPage addToCart={addToCart} />} />
+        <Route path="/cart" element={<CartPage cart={cart} />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
