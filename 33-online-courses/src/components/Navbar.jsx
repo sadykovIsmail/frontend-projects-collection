cb4803b73877ce2342de38d78857d8cb39506929
Ismail <ghvh8176@gmail.com>
2025-06-11 15:50:45 -0500

    navbar

---
+++ 33-online-courses/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', borderBottom: '1px solid gray' }}>
+      <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+      <Link to="/courses">Courses</Link>
+    </nav>
+  );
+}
