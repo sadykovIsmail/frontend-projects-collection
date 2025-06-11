168a1a400760a72712cb6a3893010cc18fbd86ad
Ismail <ghvh8176@gmail.com>
2025-06-11 16:33:16 -0500

    update navbar

--- 33-online-courses/src/components/Navbar.jsx
+++ 33-online-courses/src/components/Navbar.jsx
@@ -4,7 +4,8 @@ export default function Navbar() {
   return (
     <nav style={{ padding: '1rem', borderBottom: '1px solid gray' }}>
       <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
-      <Link to="/courses">Courses</Link>
+      <Link to="/courses" style={{ marginRight: '1rem' }}>Courses</Link>
+      <Link to="/history">History</Link>
     </nav>
   );
 }
