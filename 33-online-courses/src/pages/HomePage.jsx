814dfb250accf64199c36cd95ea0791e04f7e264
Ismail <ghvh8176@gmail.com>
2025-06-11 15:46:43 -0500

    homePage

---
+++ 33-online-courses/src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h2>Welcome to LearnX</h2>
+      <p>Explore high-quality online courses in various fields.</p>
+    </div>
+  );
+}
