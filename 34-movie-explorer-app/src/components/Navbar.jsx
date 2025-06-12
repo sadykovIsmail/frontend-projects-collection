f17d07cd8bfad0c98fe8d0b002a8c8caad1272ca
Ismail <ghvh8176@gmail.com>
2025-06-12 09:11:10 -0500

    style

--- 34-movie-explorer-app/src/components/Navbar.jsx
+++ 34-movie-explorer-app/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav style={{ padding: '1rem', borderBottom: '1px solid gray' }}>
-      <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
-      <Link to="/favourite" style={{ marginRight: '1rem' }}>Favourite</Link>
+      <Link to="/" style={{ marginRight: '1rem' }}>Home</Link> |
+      <Link to="/favourite" style={{ marginLeft: '1rem' }}>Favourite</Link>
       
     </nav>
   );
