a7b6d569ee5f65561de4c601df2ac062d9232259
Ismail <ghvh8176@gmail.com>
2025-06-18 16:12:34 -0500

    install packages

---
+++ 37-image-scrolling/src/App.jsx
@@ -0,0 +1,54 @@
+import { useRef } from 'react';
+
+export default function CatFriends() {
+  const listRef = useRef(null);
+
+  function scrollToIndex(index) {
+    const listNode = listRef.current;
+    // This line assumes a particular DOM structure:
+    const imgNode = listNode.querySelectorAll('li > img')[index];
+    imgNode.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center'
+    });
+  }
+
+  return (
+    <>
+      <nav>
+        <button onClick={() => scrollToIndex(0)}>
+          Neo
+        </button>
+        <button onClick={() => scrollToIndex(1)}>
+          Millie
+        </button>
+        <button onClick={() => scrollToIndex(2)}>
+          Bella
+        </button>
+      </nav>
+      <div>
+        <ul ref={listRef}>
+          <li>
+            <img
+              src="https://placecats.com/neo/300/200"
+              alt="Neo"
+            />
+          </li>
+          <li>
+            <img
+              src="https://placecats.com/millie/200/200"
+              alt="Millie"
+            />
+          </li>
+          <li>
+            <img
+              src="https://placecats.com/bella/199/200"
+              alt="Bella"
+            />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
