e881d7d10145b3be24bf63a590db62cc2809d219
Ismail <ghvh8176@gmail.com>
2025-06-18 23:02:36 -0500

    Deploy first React app to docs folder

--- 01-my-first-react-app/vite.config.js
+++ 01-my-first-react-app/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/frontend-projects-collection/',
+  base: '/frontend-projects-collection/01-my-first-react-app/',
   plugins: [react()],
 });
