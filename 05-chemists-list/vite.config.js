bf90a57618c02506ed17c7454f83b1519fa43ecf
Ismail <ghvh8176@gmail.com>
2025-06-19 08:45:40 -0500

    deploy

--- 05-chemists-list/vite.config.js
+++ 05-chemists-list/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/frontend-projects-collection/05-chemists-list/',
   plugins: [react()],
-})
+});
